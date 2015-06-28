9c3c668ecf36eaca55f310ff0cd043b7a6cbc979
Philip Munksgaard <pmunksgaard@gmail.com>
2015-06-28 22:17:07 +0200

    Initial commit, includes basic SKI-combinator handler

---
+++ lispski.asd
@@ -0,0 +1,10 @@
+;;;; lispski.asd
+
+(asdf:defsystem #:lispski
+  :description "Describe lispski here"
+  :author "Your Name <your.name@example.com>"
+  :license "Specify license here"
+  :serial t
+  :components ((:file "package")
+               (:file "lispski")))
+
---
+++ lispski.lisp
@@ -0,0 +1,49 @@
+;;;; lispski.lisp
+
+(in-package #:lispski)
+
+;;; "lispski" goes here. Hacks and glory await!
+
+(defvar s
+  (lambda (x)
+    (lambda (y)
+      (lambda (z)
+        (cons (cons x z)
+              (cons y z))))))
+
+(defvar k
+  (lambda (x)
+    (lambda (y)
+      x)))
+
+(defvar i
+  (lambda (x)
+    x))
+
+(defvar scope
+  (list
+   (cons 's s)
+   (cons 'k k)
+   (cons 'i i)))
+
+(defun ski-step (xs)
+  (cond ((atom xs)
+         xs)
+        (t
+         (let* ((x (ski-eval (car xs)))
+                (f (or (and (functionp x) x) (cdr (assoc x scope))))
+                (y (ski-eval (cdr xs))))
+           (if (null f)
+               (cons x y)
+               (funcall f y))))))
+
+(defun fix (f xs)
+  "Computer the fix-point of f with xs"
+  (let ((res (funcall f xs)))
+    (if (equal xs res)
+        res
+        (fix f res))))
+
+(defun ski-eval (terms)
+  "Evaluate the SKI terms in terms"
+  (fix #'ski-step terms))
